f86e3e9463fca44f33251aabfb405ac9a523a82a
starshinePerigee <skywing.ki@gmail.com>
2022-12-01 22:37:59 -0800

    Solution for part 1 day 1

--- advent/dec01.py
+++ advent/dec01.py
@@ -6,5 +6,11 @@ input = aocio.get_day(1)
 
 elves = input.split("\n\n")
 
+elf_totals = []
+for elf in elves:
+    elf_total = sum([int(s) for s in elf.split("\n") if len(s) > 0])
+    elf_totals += [elf_total]
 
-print('x')
+print(max(elf_totals))
+
+

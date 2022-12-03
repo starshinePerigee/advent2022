599d819cb06fb441d109c7d404f6c614f1d4ef1b
starshinePerigee <skywing.ki@gmail.com>
2022-12-02 23:04:28 -0800

    Begin part 3, now with tests!

---
+++ advent/dec03.py
@@ -0,0 +1,13 @@
+from util import aocio
+
+day_input = aocio.get_day(3)
+
+
+def part_one(input):
+    pass
+
+
+def test_part_one():
+    assert part_one(aocio.get_day(3, True)) == 157
+    print(part_one(aocio.get_day(3)))
+
--- util/aocio.py
+++ util/aocio.py
@@ -8,17 +8,24 @@ from util.cookie import cookie
 
 # thanks to https://github.com/Starwort/aoc_helper
 
-def get_day(day: int = None) -> str:
+def get_day(day: int = None, test: bool = False) -> str:
     if day is None:
         day = datetime.datetime.today().day
 
     cwd = os.getcwd()
-    target_path = os.path.join(os.path.dirname(cwd), "data", f"{day:02d}.txt")
+
+    if test:
+        target_path = os.path.join(os.path.dirname(cwd), "data", f"{day:02d}_test.txt")
+    else:
+        target_path = os.path.join(os.path.dirname(cwd), "data", f"{day:02d}.txt")
 
     if os.path.exists(target_path):
         with open(target_path) as input_file:
             input_string = input_file.read()
         return input_string
+
+    if test:
+        raise RuntimeError(f"Test file {target_path} does not exist!")
 
     url = f"https://adventofcode.com/2022/day/{day}/input"
 

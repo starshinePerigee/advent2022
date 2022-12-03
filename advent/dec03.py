2116d40ceea8a87ae2d0a2b3ff8c49eaef8f44a7
starshinePerigee <skywing.ki@gmail.com>
2022-12-02 23:25:33 -0800

    Day 3 part 1 complete!

--- advent/dec03.py
+++ advent/dec03.py
@@ -3,8 +3,25 @@ from util import aocio
 day_input = aocio.get_day(3)
 
 
-def part_one(input):
-    pass
+def parse_rucksack(s: str) -> int:
+    half = int(len(s)/2)
+    first_half = s[:half]
+    second_half = s[half:]
+    overlap = set([*first_half]) & set([*second_half])
+    priority = ord(list(overlap)[0]) - ord('a') + 1
+    # because ord(A) < ord(a), we need to reverse this:
+    if priority < 0:
+        priority = ord(list(overlap)[0]) - ord('A') + 27
+    return priority
+
+
+def part_one(input) -> int:
+    return sum([parse_rucksack(s) for s in input.split("\n") if len(s) > 0])
+
+
+def test_parse_rucksacks():
+    assert parse_rucksack('vJrwpWtwJgWrhcsFMMfFFhFp') == 16
+    assert parse_rucksack('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL') == 38
 
 
 def test_part_one():

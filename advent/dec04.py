55677b91c75efd2324b302a50cd9f17c2e39b1df
starshinePerigee <skywing.ki@gmail.com>
2022-12-03 21:58:56 -0800

    Began day 4

--- advent/dec04.py
+++ advent/dec04.py
@@ -1,28 +1,48 @@
+import re
+
+import pytest
+
 from util import aocio
 
 day_input = aocio.get_day(3)
 
 
-def check_overlap(left: tuple[int, int], right: tuple[int, int]) -> bool:
-    if left[0] <= right[0]:
-        lower = left
-        upper = right
+def check_overlap(entries: list[int]) -> bool:
+    left = entries[0:2]
+    right = entries[2:4]
+    if left[0] == right[0] or left[1] == right[1]:
+        return True
+    elif left[0] < right[0]:
+        return right[1] < left[1]
     else:
-        lower = right
-        upper = left
-
-    return upper[1] <= lower[1]
+        return left[1] < right[1]
 
 
-def test_check_overlap():
-    assert check_overlap((1, 3), (2, 3))
-    assert check_overlap((3, 7), (1, 10))
-    assert not check_overlap((2, 6), (1, 1))
-    assert not check_overlap((1, 4), (6, 9))
+@pytest.mark.parametrize(
+    "left, right, result",
+    [
+        ([1, 2], [3, 4], False),
+        ([1, 3], [1, 4], True),
+        ([1, 1], [2, 2], False),
+        ([2, 4], [1, 4], True),
+        ([3, 4], [1, 6], True),
+        ([1, 5], [2, 6], False)
+    ]
+)
+def test_check_overlap(left, right, result):
+    assert check_overlap(left + right) is result
+    assert check_overlap(right + left) is result
 
-#
-# def part_one(raw_input):
-#     total = 0
-#     for pair in raw_input.split('\n'):
-#         left, right = pair.split(',')
-#         left = tuple(left.split('-'))
+
+def part_one(raw_input):
+    total = 0
+    for pair in raw_input.split('\n'):
+        entries = re.split('[-,]', pair)
+        total += int(check_overlap(entries))
+    return total
+
+
+def test_part_one():
+    assert part_one(aocio.get_day(4, True)) == 2
+    print("")
+    print(part_one(aocio.get_day(4)))
